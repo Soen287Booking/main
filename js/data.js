35210ec0da04f38a6a7d33a00ba6b05f058e2079
massisdk10 <massi.sdk10@gmail.com>
2025-10-28 12:28:17 -0400

    Delete js directory

--- js/data.js
+++
@@ -1,5 +0,0 @@
-export async function loadRooms() {
-  const res = await fetch('./data/rooms.json', { cache: 'no-store' });
-  if (!res.ok) throw new Error('Failed to load rooms.json');
-  return res.json();
-}
